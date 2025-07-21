c55950c02bf9047bb97615cb88fd55f60c19fcb2
shivaananddev <102685790+shivaananddev@users.noreply.github.com>
2025-07-20 19:43:53 -0500

    ANPRA-01 Initial Commit

---
+++ src/main/java/com/anpra/java_design_patterns/JavaDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.anpra.java_design_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDesignPatternsApplication.class, args);
+    }
+
+}
